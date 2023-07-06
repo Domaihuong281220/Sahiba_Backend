6384b80b7808b1b25fbf3181f12e4e50da4128f2
Domaihuong281220 <80810923+Domaihuong281220@users.noreply.github.com>
2023-07-06 15:23:11 +0700

    first commit

---
+++ Backend/prisma/migrations/20230706081220_/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE `product` ADD COLUMN `description` VARCHAR(191) NULL,
+    ADD COLUMN `price` INTEGER NULL;
---
+++ Backend/prisma/seed.ts
@@ -0,0 +1,95 @@
+import { PrismaClient, Prisma } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const userData: Prisma.UserCreateInput[] = [
+  {
+    name: 'Alice',
+    email: 'alice@prisma.io',
+    posts: {
+      create: [
+        {
+          title: 'Join the Prisma Slack',
+          content: 'https://slack.prisma.io',
+          published: true,
+        },
+      ],
+    },
+  },
+  {
+    name: 'Nilu',
+    email: 'nilu@prisma.io',
+    posts: {
+      create: [
+        {
+          title: 'Follow Prisma on Twitter',
+          content: 'https://www.twitter.com/prisma',
+          published: true,
+        },
+      ],
+    },
+  },
+  {
+    name: 'Mahmoud',
+    email: 'mahmoud@prisma.io',
+    posts: {
+      create: [
+        {
+          title: 'Ask a question about Prisma on GitHub',
+          content: 'https://www.github.com/prisma/prisma/discussions',
+          published: true,
+        },
+        {
+          title: 'Prisma on YouTube',
+          content: 'https://pris.ly/youtube',
+        },
+      ],
+    },
+  },
+]
+
+const productData: Prisma.ProductCreateInput[] = [
+  {
+    name: 'Product 1',
+    price: 100,
+    description: 'Product 1 description', 
+    image: 'https://picsum.photos/200/300'
+  },
+  {
+    name: 'Product 2',
+    price: 200,
+    description: 'Product 2 description',
+    image: 'https://picsum.photos/200/300'
+  }
+]
+
+
+async function main() {
+  console.log(`Start seeding ...`)
+  for (const u of userData) {
+    const user = await prisma.user.create({
+      data: u,
+    })
+    console.log(`Created user with id: ${user.id}`)
+  }
+
+  for (const p of productData) {
+    const product = await prisma.product.create({
+      data: p,
+    })}
+  console.log(`Seeding finished.`)
+}
+
+
+
+
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
